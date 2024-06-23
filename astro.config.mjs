389928bbd6f35e8703f571afd3a3ec69d501a282
Sanyam Kapoor <im@sanyamkapoor.com>
2024-06-23 11:27:21 -0700

    upgrade packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
@@ -12,7 +13,18 @@ export default defineConfig({
       external: ["node:buffer"],
     },
   },
-  integrations: [react({ experimentalReactChildren: true }), sitemap()],
+  markdown: {
+    gfm: true,
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "vitesse-light",
+    },
+  },
+  integrations: [
+    react(),
+    mdx(),
+    sitemap(),
+  ],
   output: "server",
   adapter: cloudflare({
     imageService: "passthrough",
